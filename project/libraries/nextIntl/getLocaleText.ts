1930c0aeb1edfa46b4d7cc0eb5a6540c875cefed
FredHowden <fredhhowden@gmail.com>
2025-12-29 21:05:10 +0100

    essage=removed untrue message

---
+++ project/libraries/nextIntl/getLocaleText.ts
@@ -0,0 +1,36 @@
+
+import type { GetRequestConfigParams } from 'next-intl/server';
+import { hasLocale } from 'next-intl';
+import { getRequestConfig } from 'next-intl/server';
+import { localeRouting } from './localeRouting';
+
+async function getLocaleText(props: GetRequestConfigParams) {
+    const requestedLocale = await props.requestLocale;
+    const variableName = await props.locale; // IDK what props.locale does.
+
+    const availableLocales = localeRouting.locales
+
+    const requestedLocaleIsAvailable = hasLocale(availableLocales, requestedLocale);
+
+    if (requestedLocaleIsAvailable) {
+        const requestedLocaleText = await import(`../../locales/${requestedLocale}.json`);
+
+        return {
+            locale: requestedLocale,
+            messages: requestedLocaleText.default
+        };
+
+    } else {
+        const fallbackLocale = localeRouting.defaultLocale
+        const fallbackLocaleText = await import(`../../locales/${fallbackLocale}.json`);
+        return {
+            locale: fallbackLocale,
+            messages: fallbackLocaleText.default
+        };
+
+    }
+}
+
+
+
+export default getRequestConfig(getLocaleText);
